8c38c3287d01f802ce0a10f19a2eaf8e95175988
Mouparna Biswas <85263019+Mouparna-commit@users.noreply.github.com>
2026-04-08 23:41:04 -0700

    Add files via upload

--- Global_Operations.sql
+++ Global_Operations.sql
@@ -8,3 +8,32 @@ insert into Global_Operations.dbo.practice_01
 (order_id, customer_id, order_status, order_date, total_amount, last_updated_ts, ingest_batch_id)
 values(1003,003, 'Cancelled', '2025-01-13', 200.00, '2025-01-13 11:30:00', 105); 
 
+select * 
+from Global_Operations.dbo.practice_01; 
+
+/* Write a SQl Query to find the following 
+ one Row per Order id
+ the most recent record based on timestamp
+ if there is a tie on last_updated_ts, then pick the row with highest ingest_batch_id
+ */
+
+ with T1 as (select order_id , 
+ customer_id, 
+ order_status, 
+ order_date, 
+ total_amount, 
+ last_updated_ts, 
+ ingest_batch_id, 
+ row_number() over (partition by order_id order by last_updated_ts desc, ingest_batch_id desc )  as rn
+ from Global_operations.dbo.practice_01) 
+
+ select * 
+ from  
+ T1
+ where T1.rn = 1
+ order by order_id ; 
+
+ 
+
+
+ 
